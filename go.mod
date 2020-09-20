bd802c5f545587c2359212a3233fb64a3b2a5f58
dankox <dankon@email.cz>
2020-09-20 23:57:11 +0200

    changed TUI library to gocui
    created some simple example to test how it works

--- go.mod
+++ go.mod
@@ -3,5 +3,7 @@ module github.com/dankox/zmonitor-go
 go 1.15
 
 require (
-	github.com/gizak/termui/v3 v3.1.0
+	github.com/jroimartin/gocui v0.4.0
+	github.com/mattn/go-runewidth v0.0.9 // indirect
+	github.com/nsf/termbox-go v0.0.0-20200418040025-38ba6e5628f1 // indirect
 )
--- main.go
+++ main.go
@@ -1,32 +1,80 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"math/rand"
+	"time"
 
-	ui "github.com/gizak/termui/v3"
-	"github.com/gizak/termui/v3/widgets"
+	"github.com/jroimartin/gocui"
 )
 
+// go always executes init() at the startup, after all variable declarations
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
 func main() {
-	if err := ui.Init(); err != nil {
-		log.Fatalf("failed to initialize termui: %v", err)
+	g, err := gocui.NewGui(gocui.OutputNormal)
+	if err != nil {
+		log.Panicln(err)
 	}
-	defer ui.Close()
+	defer g.Close()
 
-	p := widgets.NewParagraph()
-	p.Title = "zMonitor"
-	p.TitleStyle.Fg = ui.ColorGreen
-	p.Text = "[Hello](fg:red) World"
-	p.TextStyle = ui.NewStyle(ui.ColorYellow)
-	p.SetRect(0, 0, 25, 5)
-	p.BorderStyle.Fg = ui.ColorGreen
-	// p.BorderStyle.Modifier = ui.ModifierReverse
+	g.SetManagerFunc(layout)
 
-	ui.Render(p)
+	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
+		log.Panicln(err)
+	}
 
-	for e := range ui.PollEvents() {
-		if e.Type == ui.KeyboardEvent {
-			break
+	if err := g.SetKeybinding("", gocui.KeyCtrlR, gocui.ModNone, updateLayout); err != nil {
+		log.Panicln(err)
+	}
+
+	if err := g.MainLoop(); err != nil && err != gocui.ErrQuit {
+		log.Panicln(err)
+	}
+}
+
+// Layout function is always called at the end of MainLoop(), so be careful to not update layout outside of it
+//  - consume events (like keypress, mouse, resize) and user events (from gocui.Update())
+//  - flush () -> calls layout function
+func layout(g *gocui.Gui) error {
+	maxX, maxY := g.Size()
+	if _, err := g.View("zMonitor"); err == gocui.ErrUnknownView {
+		if v, err := g.SetView("zMonitor", maxX/2-7, maxY/2, maxX/2+7, maxY/2+2); err != nil {
+			if err != gocui.ErrUnknownView {
+				return err
+			}
+			fmt.Fprintln(v, "Hello world!")
 		}
 	}
+	return nil
+}
+
+func updateLayout(g *gocui.Gui, v *gocui.View) error {
+
+	// gocui.Update() can be called from goroutine to update content
+	go g.Update(func(g *gocui.Gui) error {
+		name := "zMonitor"
+		maxX, maxY := g.Size()
+		delta := rand.Intn(10)
+		nv, err := g.SetView(name, maxX/2-(7+delta), maxY/2-delta, maxX/2+7+delta, maxY/2+2+delta)
+		if err != nil {
+			fmt.Printf("zMonitor error: %v", err)
+			return err
+		}
+		nv.Wrap = true
+		g.SetCurrentView(name)
+		g.SetViewOnTop(name)
+		nv.Clear()
+		fmt.Fprintln(nv, "Hello random z/OS world!")
+		return nil
+	})
+
+	return nil
+}
+
+func quit(g *gocui.Gui, v *gocui.View) error {
+	return gocui.ErrQuit
 }
